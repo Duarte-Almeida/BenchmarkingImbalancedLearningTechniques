0130e238b9d6bcbadf1a1b3123a3ae3c1fa353da
Duarte-Almeida <duartecaladoalmeida@gmail.com>
2023-12-18 03:43:45 +0000

    intermediate commit

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ import sys
 import utils
 import oversamplers
 import undersamplers
+import hybrid
+import thresholds
 from losses import LabelSmoothingLoss, FocalLoss
 from classifiers import LGBM
+from CustomCV import CustomCV
 
 import lightgbm as lgb
 import numpy as np
@@ -25,46 +28,27 @@ import sklearn.model_selection
 from sklearn import metrics
 
 import matplotlib.pyplot as plt
-from lightgbm import LGBMClassifier
-import sklearn.metrics
 
-
-def f1_measure(y_test, y_pred):
-    TP = np.sum(y_pred[y_test == 1.0])
-    TN = np.sum(1 - y_pred[y_test == 0])
-    FP = np.sum(y_pred[y_test == 0])
-    FN = np.sum(1 - y_pred[y_test == 1.0])
-
-    precision = TP / (TP + FP)
-    recall = TP / (TP + FN)
-    
-    if precision == 0 or recall == 0:
-        return 0
-    else:
-        return (2 * precision * recall) / (precision + recall)
 
 def auc(y_test, y_pred):    
     return sklearn.metrics.roc_auc_score(y_test, y_pred)
-
-def compute_cutoff(y_test, y_pred):
-    fpr, tpr, th = sklearn.metrics.roc_curve(y_test, y_pred)
-    J_statistic = tpr + 1 - fpr
-    return th[np.argmax(J_statistic)]
-
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-dataset',
                         choices=['baf'],
                         help="Which dataset should we choose?")
+    parser.add_argument('-data_subsampling', default = 1.00,
+                        type = float,
+                        help="By how much should the dataset be reduced?")
     parser.add_argument('-oversampling', default = None,
-                        choices=['SMOTE', 'SMOTENC', 'ADASYN', 'KMeansSMOTE'],
+                        choices=['SMOTE', 'ADASYN', 'KMeansSMOTE', 'RACOG'],
                         help="Which oversampling strategy should we choose?")
     parser.add_argument('-undersampling', default = None,
-                        choices=['RUS'],
+                        choices=['RUS', 'TomekLinks', 'ENN'],
                         help="Which undersampling strategy should we choose?")
     parser.add_argument('-hybrid', default = None,
-                        choices=['TODO'],
+                        choices=['IPF'],
                         help="Which hybrid oversampling/undersammpling strategy should we choose?")
     parser.add_argument('-label_smoothing', default = False,
                         action="store_true",
@@ -72,6 +56,9 @@ def main():
     parser.add_argument('-plot_scores', default = False,
                         action="store_true",
                         help="Should we perform label smoothing?")
+    parser.add_argument('-threshold', default = 'ROC',
+                        choices=['CS', 'ROC'],
+                        help="Which threshold-moving strategy should we choose?")
     
     opt = parser.parse_args()
 
@@ -81,9 +68,16 @@ def main():
     X_train, y_train = data["train"]
     X_test, y_test = data["test"]
 
-    #sys.exit()
-
     cat_feats = data["cat_feats"]
+
+    np.random.seed(42)
+    X_train = X_train.reset_index(drop = True)
+    y_train = y_train.reset_index(drop = True)
+    if opt.data_subsampling < 1.00:
+        X_train = X_train.reset_index(drop = True)
+        y_train = y_train.reset_index(drop = True)
+        sub_idx = np.random.choice(X_train.shape[0], size = int(X_train.shape[0] * opt.data_subsampling), replace = False)
+        X_train, y_train = X_train.loc[sub_idx], y_train.loc[sub_idx]
 
     steps = []
     param_grid = {}
@@ -95,12 +89,11 @@ def main():
         print("Cannot perform oversampling and undersampling approaches simulaneously! Check hybrid option instead.")
         sys.exit(-1)
     
-    if opt.hybrid is not None and opt.undersampling is not None:
+    if opt.hybrid is not None and opt.oversampling is None:
         print("Can only use hybrid sampling if oversampling strategy is defined")
         sys.exit(-1)
     
-    if opt.oversampling is not None:
-        print("Will perform oversampling!")
+    if opt.oversampling is not None and opt.hybrid is None:
         preprocessor = oversamplers.fetch_oversampler(opt.oversampling)
         steps = steps + [("preprocessor", preprocessor)]
 
@@ -109,9 +102,9 @@ def main():
                 param_grid["preprocessor__" + str(parameter)] = values
 
         name += f"_{opt.oversampling}"
+        preprocessor.set_params(**{"categorical_features":len(cat_feats)})
 
     if opt.undersampling is not None:
-        print("Will perform undersampling!")
         preprocessor = undersamplers.fetch_undersampler(opt.undersampling)
         steps = steps + [("preprocessor", preprocessor)]
 
@@ -119,6 +112,19 @@ def main():
             for (parameter, values) in preprocessor.parameter_grid().items():
                 param_grid["preprocessor__" + str(parameter)] = values
         name += f"_{opt.undersampling}"
+        preprocessor.set_params(**{"categorical_features":len(cat_feats)})
+
+    
+    if opt.hybrid is not None:
+        preprocessor = hybrid.fetch_hybrid(opt.hybrid)
+        steps = steps + [("preprocessor", preprocessor)]
+
+        if preprocessor.parameter_grid() is not None:
+            for (parameter, values) in preprocessor.parameter_grid().items():
+                param_grid["preprocessor__" + str(parameter)] = values
+        name += f"_{opt.undersampling}"
+        preprocessor.set_params(**{"categorical_features":len(cat_feats)})
+
 
     if opt.label_smoothing:
         name += "_LS"
@@ -128,39 +134,40 @@ def main():
     else:
         ls = LabelSmoothingLoss(0, freeze = True)
         clf = LGBM(ls)
-    #preprocessor.set_params(**{"categorical_features":cat_feats})
-    #print(getattr(preprocessor, "categorical_features"))
+
     steps = steps + [("clf", clf)]
     for (parameter, values) in clf.parameter_grid().items():
         param_grid["clf__" + str(parameter)] = values
     pipeline = Pipeline(steps=steps)
+    if "preprocessor" in pipeline.named_steps:
+        prep_name = "preprocessor"
+    else:
+        prep_name = None
+
     start_time = time.time()
     print('Starting training...')
-    search = GridSearchCV(pipeline, param_grid, scoring = make_scorer(auc, greater_is_better=True, needs_proba = True), 
-                            n_jobs=1, verbose = 4, error_score = "raise", refit = True, cv = 2)
-    search.fit(X_train, y_train)
-    print("Best parameter (CV score=%0.3f):" % search.best_score_)
-    print(search.best_params_)
+    is_oversampler = opt.oversampling is not None
+    is_undersampler = opt.undersampling is not None
+    search = CustomCV(prep_name = prep_name, estimator = pipeline, param_distributions = param_grid,
+                      scoring = make_scorer(auc, greater_is_better=True, needs_proba = True), 
+                      n_jobs=1, verbose = 2, error_score = "raise", refit = False, cv = 2,
+                      oversampler = is_oversampler, undersampler = is_undersampler, n_iter = 10, random_state = 42)
+                      
+    search.fit(X_train, y_train, clf__categorical_features = len(cat_feats))
+
     probs = search.predict_proba(X_test)[:, 1]
-    test_probs = search.predict_proba(X_train)[:, 1]
+    train_probs = search.predict_proba(X_train)[:, 1]
 
-    th = compute_cutoff(y_train, test_probs)
+    th = thresholds.fetch_threshold(opt.threshold, y_train, train_probs)
     y_pred = search.best_estimator_.predict(X_test, **{"th":th})
-
-    #y_pred = pipeline["clf"].predict(X_test)
-    #probs = pipeline["clf"].predict_proba(X_test)
 
     end_time = time.time()
     total_time = end_time - start_time
     print(f'Ended training and classification in {round(end_time - start_time, 2)} seconds.\n')
 
-    #display = sklearn.metrics.RocCurveDisplay.from_predictions(y_test, probs)
-    #plt.show()
     utils.dump_metrics(y_test, y_pred, probs, name, total_time)
 
     if opt.plot_scores:
-        #probs = search.best_estimator_["clf"].predict_proba(X_test)
-        #probs = pipeline["clf"].predict_proba(X_test)[:, 1]
         positives = probs[y_test == 1]
         negatives = probs[y_test == 0]
         plt.hist(probs[y_test == 1], bins = 100, range = (0, 1), label = "Positive", alpha = 0.5, weights = np.ones_like(positives)/float(len(positives)))
